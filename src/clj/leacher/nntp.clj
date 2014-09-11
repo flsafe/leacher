c584d4f1c0316e196c1657eae1fcba82496e5696
Gareth Jones <gareth.e.jones@gmail.com>
2014-09-11 17:23:11 +0100

    all broken!

--- src/clj/leacher/nntp.clj
+++ src/clj/leacher/nntp.clj
@@ -14,6 +14,18 @@
 
 (declare response)
 
+(defn connect
+  [conn]
+  (let [{:keys [host port ssl?]} (:opts conn)
+        socket (if ssl?
+                 (.createSocket (SSLSocketFactory/getDefault)
+                   ^String host ^int port)
+                 (Socket. ^String host ^Long port))]
+    (reset! conn {:socket socket
+                  :in     (io/reader socket :encoding ENCODING)
+                  :out    (io/writer socket :encoding ENCODING)})
+    (response @(:conn conn))))
+
 (defn write
   [conn ^String msg]
   (doto ^Writer (:out conn)
@@ -73,84 +85,70 @@
             (.write b c)
             (recur c)))))))
 
-(defprotocol NntpConnection
-  (connect [this])
-  (re-connect [this])
-  (with-reconnect-if-timeout [this body-fn])
-  (close [this])
-  (authenticate [this])
-  (group [this name-of])
-  (article [this message-id]))
+(defn re-connect
+  [conn]
+  (connect conn)
+  (when @(:current-group conn)
+    (group @(:conn conn) @(:current-group conn))))
+
+(defn with-reconnect-if-timeout
+  [conn body-fn]
+  (try
+    (body-fn)
+    (catch clojure.lang.ExceptionInfo e
+      (if (= 400 (-> e ex-data :code))
+        (do (println "idle timeout, reconnecting")
+            (re-connect conn)
+            (body-fn))
+        (throw e)))))
+
+(defn authenticate
+  [conn]
+  (with-reconnect-if-timeout conn
+    (fn []
+      (let [{:keys [user password]} (:opts conn)]
+        (when (and user password)
+          (write @(:conn conn) (str "AUTHINFO USER " user))
+          (let [resp (response @(:conn conn))]
+            (when (<= 300 (:code resp) 399)
+              (write @(:conn conn) (str "AUTHINFO PASS " password))
+              (response @(:conn conn)))))))))
+
+(defn group
+  [conn group-name]
+  (reset! (:current-group conn) group-name)
+  (with-reconnect-if-timeout conn
+    (fn []
+      (write @(:conn conn) (str "GROUP " group-name))
+      (let [resp              (response @(:conn conn))
+            [number low high] (string/split (:body resp) #" ")]
+        (assoc resp
+          :number (parse-long number)
+          :low    (parse-long low)
+          :high   (parse-long high)
+          :group  group-name)))))
+
+(defn article
+  [conn message-id]
+  (with-reconnect-if-timeout conn
+    (fn []
+      (write @(:conn conn) (str "ARTICLE " message-id))
+      (let [resp    (response @(:conn conn))
+            headers (header-response @(:conn conn))
+            bytes   (byte-body-response @(:conn conn))]
+        (assoc resp
+          :headers headers
+          :bytes   bytes
+          :size    (count bytes))))))
+
+
+
+(defrecord NntpConnection [conn current-group opts])
 
 (defn new-connection
   [opts]
   (let [conn          (atom nil)
         current-group (atom nil)]
-    (reify NntpConnection
-      
-      (connect [this]
-        (let [{:keys [host port ssl?]} opts
-              socket (if ssl?
-                       (.createSocket (SSLSocketFactory/getDefault)
-                         ^String host ^int port)
-                       (Socket. ^String host ^Long port))]
-          (reset! conn {:socket socket
-                        :in     (io/reader socket :encoding ENCODING)
-                        :out    (io/writer socket :encoding ENCODING)})
-          (response @conn)))
-
-      (re-connect [this]
-        (connect this)
-        (when @current-group
-          (group this @current-group)))
-
-      (with-reconnect-if-timeout [this body-fn]
-        (try
-          (body-fn)
-          (catch clojure.lang.ExceptionInfo e
-            (if (= 400 (-> e ex-data :code))
-              (do (println "idle timeout, reconnecting")
-                  (re-connect this)
-                  (body-fn))
-              (throw e)))))
-
-      (authenticate [this]
-        (with-reconnect-if-timeout this
-          (fn []
-            (let [{:keys [user password]} opts]
-              (when (and user password)
-                (write @conn (str "AUTHINFO USER " user))
-                (let [resp (response @conn)]
-                  (when (<= 300 (:code resp) 399)
-                    (write @conn (str "AUTHINFO PASS " password))
-                    (response @conn))))))))
-
-      (group [this group-name]
-        (reset! current-group group-name)
-        (with-reconnect-if-timeout this
-          (fn []
-            (write @conn (str "GROUP " group-name))
-            (let [resp              (response @conn)
-                  [number low high] (string/split (:body resp) #" ")]
-              (assoc resp
-                :number (parse-long number)
-                :low    (parse-long low)
-                :high   (parse-long high)
-                :group  group-name)))))
-
-      (article [this message-id]
-        (with-reconnect-if-timeout this
-          (fn []
-            (write @conn (str "ARTICLE " message-id))
-            (let [resp    (response @conn)
-                  headers (header-response @conn)
-                  bytes   (byte-body-response conn)]
-              (assoc resp
-                :headers headers
-                :bytes   bytes
-                :size    (count bytes))))))
-
-      (close [this]
-        (.close ^Socket (:socket @conn))))))
-
-
+    (map->NntpConnection {:conn conn
+                          :current-group current-group
+                          :opts opts})))
